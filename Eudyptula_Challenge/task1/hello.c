cd6f9b6ffb27b205f74e80761da6a745dda5522c
Devioslang <jxth152913@163.com>
2014-03-20 20:04:21 +0800

    add eudyptula chanlenge

---
+++ Eudyptula_Challenge/task1/hello.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+
+MODULE_AUTHOR("devioslang");
+MODULE_LICENSE("GPL");
+
+
+static int __init vmx_init(void)
+{
+    printk(KERN_DEBUG "hello kernel world\n");
+    return 0;
+}
+
+static void __exit vmx_exit(void)
+{
+    printk("bye kernel\n");
+}
+
+module_init(vmx_init);
+module_exit(vmx_exit);
